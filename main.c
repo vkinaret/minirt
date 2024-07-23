9866c23b19e7106e6c629d7929e2466d8be0f5ff
Safa Tuna <56443802+tunasafa@users.noreply.github.com>
2024-07-23 17:13:13 +0300

    Update main.c

--- main.c
+++ main.c
@@ -5,32 +5,36 @@ int main() {
     mlx_image_t* img;
     uint32_t sphere_color = 0xFFFFF;
     uint32_t cylinder_color = 0xFFFFFF;
+	uint32_t plane_color = 0x0000FF;
+
 
     // Initialize mlx
-    mlx_ptr = mlx_init(800, 600, "3D Sphere", false);
+    mlx_ptr = mlx_init(1600, 1200, "3D Sphere", false);
     if (!mlx_ptr) {
         return 1;
     }
 
     // Create a new image
-    img = mlx_new_image(mlx_ptr, 800, 600);
+    img = mlx_new_image(mlx_ptr, 1600, 1200);
     if (!img) {
         mlx_terminate(mlx_ptr);
         return 1;
     }
 
     // Light source position
-    int light_x = -10000;
+    int light_x = 10000;
     int light_y = 0;
     int light_z = -20000;
 
     // Camera parameters
-    float aspect_ratio = 800.0 / 600.0; // Width / Height
+    float aspect_ratio = 1600.0 / 1200.0; // Width / Height
     float fov = 100.0; // Field of view in degrees
+
+	render_plane(img, plane_color, light_x, light_y, light_z , aspect_ratio, fov);
 
     render_sphere(img, sphere_color, light_x, light_y, light_z, aspect_ratio, fov);
     render_cylinder(img, cylinder_color, light_x, light_y, light_z, aspect_ratio, fov);
-    
+	
     // Display the image on the window
     mlx_image_to_window(mlx_ptr, img, 0, 0);
 
