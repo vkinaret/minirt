90b47b957cd41f4b7886d8c6b80a52aa6f921a5b
Safa Tuna <56443802+tunasafa@users.noreply.github.com>
2024-07-26 17:40:18 +0300

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main() {
     mlx_image_t* img;
     uint32_t sphere_color = 0xFFFFF;
     uint32_t cylinder_color = 0xFFFFFF;
-	uint32_t plane_color = 0x0000FF;
+	uint32_t plane_color = 0xFFFFFF;
 
 
     // Initialize mlx
@@ -24,17 +24,20 @@ int main() {
     // Light source position
     int light_x = 10000;
     int light_y = 0;
-    int light_z = -20000;
+    int light_z = -2000;
+	float ambient_intensity = 0.5f; // Example value
+    float ambient_reflectivity = 0.9f; // Example value
 
     // Camera parameters
     float aspect_ratio = 1600.0 / 1200.0; // Width / Height
     float fov = 100.0; // Field of view in degrees
-
-	render_plane(img, plane_color, light_x, light_y, light_z , aspect_ratio, fov);
-
-    render_sphere(img, sphere_color, light_x, light_y, light_z, aspect_ratio, fov);
-    render_cylinder(img, cylinder_color, light_x, light_y, light_z, aspect_ratio, fov);
 	
+	render_plane(img, plane_color, aspect_ratio, fov);
+
+
+    render_sphere(img, sphere_color, light_x, light_y, light_z, aspect_ratio, fov, ambient_intensity, ambient_reflectivity);
+	render_cylinder(img, cylinder_color, light_x, light_y, light_z, aspect_ratio, fov, ambient_intensity, ambient_reflectivity);
+
     // Display the image on the window
     mlx_image_to_window(mlx_ptr, img, 0, 0);
 
