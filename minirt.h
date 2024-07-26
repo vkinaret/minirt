96e4d6a3d895274fab9f3bccd4bfdb875ccf55e5
Safa Tuna <56443802+tunasafa@users.noreply.github.com>
2024-07-26 17:40:45 +0300

    Update minirt.h

--- minirt.h
+++ minirt.h
@@ -3,18 +3,27 @@
 
 #include "MLX42/include/MLX42/MLX42.h"
 #include <stdint.h>
+#include <stdlib.h>
 
-// Function to calculate the color of a point on the sphere under lighting
-uint32_t calculate_lighting(float x, float y, float z, uint32_t sphere_color, int light_x, int light_y, int light_z);
+
+uint32_t calculate_lighting(float x, float y, float z, uint32_t sphere_color, int light_x, int light_y, int light_z, float ambient_intensity, float ambient_reflectivity);
 
 // Function to render a 3D sphere
-void render_sphere(mlx_image_t* img, uint32_t sphere_color, int light_x, int light_y, int light_z, float aspect_ratio, float fov);
+void render_sphere(mlx_image_t* img, uint32_t sphere_color, int light_x, int light_y, int light_z, float aspect_ratio, float fov,float ambient_intensity,float ambient_reflectivity);
 
 // Function to render a 3D cylinder
-void render_cylinder(mlx_image_t* img, uint32_t cylinder_color, int light_x, int light_y, int light_z, float aspect_ratio, float fov);
-/*
-void render_plane(mlx_image_t* img, uint32_t plane_color, float aspect_ratio, float fov);*/
+void render_cylinder(mlx_image_t* img, uint32_t cylinder_color, int light_x, int light_y, int light_z, float aspect_ratio, float fov,float ambient_intensity,float ambient_reflectivity);
 
-void render_plane(mlx_image_t* img, uint32_t plane_color, int light_x, int light_y, int light_z, float aspect_ratio, float fov);
+
+
+
+void render_plane(mlx_image_t* img, uint32_t plane_color, float aspect_ratio, float fov);
+
+
+
+
+
+
+
 
 #endif // RENDER_H
