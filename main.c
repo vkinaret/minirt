0a4b5085ee8defae4aaf24827b780688f004bd3c
Safa Tuna <56443802+tunasafa@users.noreply.github.com>
2024-08-09 16:33:13 +0300

    Update main.c

--- main.c
+++ main.c
@@ -5,38 +5,42 @@ int main() {
     mlx_image_t* img;
     uint32_t sphere_color = 0xFFFFF;
     uint32_t cylinder_color = 0xFFFFFF;
-	uint32_t plane_color = 0xFFFFFF;
+    //uint32_t plane_color = 0xFFFFFF;
 
 
     // Initialize mlx
-    mlx_ptr = mlx_init(1600, 1200, "3D Sphere", false);
+    mlx_ptr = mlx_init(900, 600, "3D Sphere", false);
     if (!mlx_ptr) {
         return 1;
     }
 
     // Create a new image
-    img = mlx_new_image(mlx_ptr, 1600, 1200);
+    img = mlx_new_image(mlx_ptr, 900, 600);
     if (!img) {
         mlx_terminate(mlx_ptr);
         return 1;
     }
 
     // Light source position
-    int light_x = 10000;
-    int light_y = 0;
-    int light_z = -2000;
-	float ambient_intensity = 0.5f; // Example value
-    float ambient_reflectivity = 0.9f; // Example value
+    int light_x = -2000;
+    int light_y = -100;
+    int light_z = -100;
+    float ambient_intensity = 0.6f; // Example value
+    float ambient_reflectivity = 0.7f; // Example value
 
     // Camera parameters
+    float camera_x = 0.0; // Set your desired camera position
+    float camera_y = -100;
+    float camera_z = -300;
+
     float aspect_ratio = 1600.0 / 1200.0; // Width / Height
-    float fov = 100.0; // Field of view in degrees
-	
-	render_plane(img, plane_color, aspect_ratio, fov);
+    float fov = 90.0; // Field of view in degrees
+
+    //render_plane(img, plane_color, aspect_ratio, fov);
 
 
-    render_sphere(img, sphere_color, light_x, light_y, light_z, aspect_ratio, fov, ambient_intensity, ambient_reflectivity);
-	render_cylinder(img, cylinder_color, light_x, light_y, light_z, aspect_ratio, fov, ambient_intensity, ambient_reflectivity);
+    render_sphere(img, sphere_color, light_x, light_y, light_z, aspect_ratio, fov, ambient_intensity, ambient_reflectivity, camera_x, camera_y, camera_z);
+    render_cylinder(img, cylinder_color, light_x, light_y, light_z, aspect_ratio, fov, ambient_intensity, ambient_reflectivity, camera_x, camera_y, camera_z);
 
     // Display the image on the window
     mlx_image_to_window(mlx_ptr, img, 0, 0);
